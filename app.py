ef4a6135f67b6365140d624aaecedb84787f765e
Roy C <61641010+canacosmic@users.noreply.github.com>
2023-10-25 16:04:15 -0600

    mas avance
    clasifique manualmente los sintomas en su categoria. me decidi por 6 ategorias

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask, render_template, url_for
+from model import cargar_datos_entrenamiento, entrenar_modelo
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+# Importa las funciones definidas en model.py
+
+# Cargar datos de entrenamiento y entrenar el modelo al iniciar la aplicación
+sintomas, diagnosticos, tratamientos = cargar_datos_entrenamiento()
+modelo = entrenar_modelo(sintomas, diagnosticos)
 
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+
+@app.route('/procesar', methods=['POST'])
+def procesar():
+    # Obtener los síntomas seleccionados por el usuario desde el formulario
+    sintomas_usuario = request.form.getlist('sintomas')
+
+    # Realizar una predicción con el modelo
+    diagnosticos_sugeridos = []
+
+    for i, sintomas_enfermedad in enumerate(sintomas):
+        # Verificar si los síntomas del usuario coinciden con los síntomas de una enfermedad
+        if set(sintomas_usuario).issubset(sintomas_enfermedad):
+            diagnosticos_sugeridos.append(diagnosticos[i])
+
+    # Mostrar los diagnósticos sugeridos al usuario
+    return render_template('resultado.html', diagnosticos=diagnosticos_sugeridos)
 
 
 if __name__ == "__main__":
--- model.py
+++ model.py
@@ -1,6 +1,70 @@
 # modelo.py
 from sklearn.ensemble import RandomForestClassifier
 
+# Aqui es donde clasifico cada sintoma segun su categoría
+
+# sintomas de comportameinto
+sintomas_comportamiento = [
+    "Inactivity",
+    "Erratic swimming",
+    "Sluggishness",
+    "Weight loss",
+    "Loss of color or appetite",
+    "Bloating or swelling in the body",
+    "Abnormal swimming",
+    "Darting",
+    "Flashing",
+    "Gasping for air",
+    "Unusually bulging of one or both eyes",
+    "Fish struggles to swim, may float with head tipped down, or have difficulty surfacing, no balance, etc. May occur after eating",
+    "Sudden abnormal behavior"
+]
+
+
+# sintomas de excremento
+sintomas_excremento = [
+    "String of feces hanging from fish",
+]
+
+# sintomas del cuerpo
+sintomas_body = [
+    "Grayish-white film on skin",
+    "White or gray fungus on eyes",
+    "Bluish-white film on body",
+    "Reddened areas on body",
+    "Small dark spots on fins and body",
+    "Red or bloody gills",
+    "Cloudy white appearance to one or both eyes",
+    "Reddening on or under skin"
+]
+
+
+# sintomas de las aleetas
+sintomas_fins = [
+    "Damaged fins",
+    "Ragged or decaying fins",
+    "Small white spots that get larger over time, possibly with black streaks"
+]
+
+# sintomas de las branquias
+sintomas_branquias = [
+    "Yellow to gray patches on gills",
+    "Strained breathing caused by gill damage",
+    "Difficulty breathing due to gill damage"
+]
+
+# sintomas de la cabeza
+sintomas_cabeza = [
+    "Tissue on the head may be eaten away",
+    "Swelling of head",
+    "Bulging eyes",
+    "Scratching, white salt-like spots starting on the head and spreading over the whole body, rapid breathing, cloudiness on eyes or fins",
+    "Scratching, small worms hanging from the body",
+    "Scratching, green to brown lice (up to an inch) visible on the skin",
+    "Small yellow to white spots dusting the skin",
+    "Small holes or eroding pits appearing in the head"
+]
+
 
 def cargar_datos_entrenamiento():
     tabla_de_datos = [
